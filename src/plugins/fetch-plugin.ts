a0dfa598ef4821103770b1228201ebb1103fd9da
mofath <mohamed.elgaedi@gmail.com>
2021-03-12 18:59:52 +0200

    Load css files

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -1,25 +1,47 @@
-import * as esbuild from 'esbuild-wasm';
-import axios from 'axios';
+import * as esbuild from "esbuild-wasm";
+import axios from "axios";
 
 export const fetchPlugin = (inputCode: string) => {
   return {
-    name: 'fetch-plugin',
+    name: "fetch-plugin",
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: inputCode,
-          };
-        }
+      build.onLoad({ filter: /(^index\.js$)/ }, () => {
+        return {
+          loader: "jsx",
+          contents: inputCode,
+        };
+      });
 
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        const { data, request } = await axios.get(args.path);
+        const escaped = data
+          .replace(/\n/g, "")
+          .replace(/"/g, '\\"')
+          .replace(/'/g, "\\'");
+        const contents = `
+          const style = document.createElement('style');
+          style.innerText = '${escaped}';
+          document.head.appendChild(style);
+        `;
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+
+        return result;
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
         const { data, request } = await axios.get(args.path);
 
         const result: esbuild.OnLoadResult = {
-          loader: 'jsx',
+          loader: "jsx",
           contents: data,
-          resolveDir: new URL('./', request.responseURL).pathname,
+          resolveDir: new URL("./", request.responseURL).pathname,
         };
+
         return result;
       });
     },
