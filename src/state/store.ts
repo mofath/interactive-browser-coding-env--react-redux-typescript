201753e45fc220c963e8ad230fb0d402cd359d63
mofath <mohamed.elgaedi@gmail.com>
2021-03-15 21:55:28 +0200

    Code cleanup: remove old store

--- src/state/store.ts
+++
@@ -1,5 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import reucers from "./reducer";
-
-export const store = createStore(reucers, {}, applyMiddleware(thunk));
