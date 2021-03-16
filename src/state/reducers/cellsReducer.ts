e071383d3f9fa6455fbe1faddf6a1b236c9289d2
mofath <mohamed.elgaedi@gmail.com>
2021-03-16 22:31:05 +0200

    Add dummy export at bundleReducer

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,7 +1,7 @@
-import produce from 'immer';
-import { ActionType } from '../action-types';
-import { Action } from '../actions';
-import { Cell } from '../cell';
+import produce from "immer";
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+import { Cell } from "../cell";
 
 interface CellsState {
   loading: boolean;
@@ -25,28 +25,28 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
       const { id, content } = action.payload;
 
       state.data[id].content = content;
-      return;
+      return state;
     case ActionType.DELETE_CELL:
       delete state.data[action.payload];
       state.order = state.order.filter((id) => id !== action.payload);
 
-      return;
+      return state;
     case ActionType.MOVE_CELL:
       const { direction } = action.payload;
       const index = state.order.findIndex((id) => id === action.payload.id);
-      const targetIndex = direction === 'up' ? index - 1 : index + 1;
+      const targetIndex = direction === "up" ? index - 1 : index + 1;
 
       if (targetIndex < 0 || targetIndex > state.order.length - 1) {
-        return;
+        return state;
       }
 
       state.order[index] = state.order[targetIndex];
       state.order[targetIndex] = action.payload.id;
 
-      return;
+      return state;
     case ActionType.INSERT_CELL_BEFORE:
       const cell: Cell = {
-        content: '',
+        content: "",
         type: action.payload.type,
         id: randomId(),
       };
