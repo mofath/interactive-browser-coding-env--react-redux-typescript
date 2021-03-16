b24e9e834491503cb67980b64574b55f91d7bf8c
mofath <mohamed.elgaedi@gmail.com>
2021-03-16 20:53:25 +0200

    Setup store

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import ReactDOM from "react-dom";
-// import CodeCell from "./components/code-cell";
+import { Provider } from "react-redux";
+import { store } from "./state";
 import TextEditor from "./components/text-editor";
+// import CodeCell from "./components/code-cell";
 
 const App = () => {
   return (
     <div>
-      <TextEditor />
+      <Provider store={store}>
+        <TextEditor />
+      </Provider>
       {/* <CodeCell /> */}
     </div>
   );
